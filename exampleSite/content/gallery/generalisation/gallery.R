d3cb533d1840744b38599a28c46df07adf33dbec
Danielle Navarro <d.navarro@unsw.edu.au>
2020-09-15 15:49:34 +1000

    pushes gallery div inside call to make_gallery

--- exampleSite/content/gallery/generalisation/gallery.R
+++ exampleSite/content/gallery/generalisation/gallery.R
@@ -1,5 +1,8 @@
 make_gallery <- function(preview = "preview", full = "image") {
   images <- list.files(preview, pattern="png$")
   links <- paste0('<a href="', full,'/',images,'"><img src="',preview,'/',images,'"></a>')
+
+  cat('<div class="gal">')
   cat(paste(links, collapse="\n"))
+  cat('</div>\n')
 }
