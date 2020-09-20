9a7f630df5b5b64d18898879c246d539844d4620
Danielle Navarro <d.navarro@unsw.edu.au>
2020-09-20 12:50:32 +1000

    adds make_previews, attempts switching make_galleries to self-contained bootstrap

--- exampleSite/content/diziet.R
+++ exampleSite/content/diziet.R
@@ -1,17 +1,44 @@
 diziet <- list(
 
-  make_gallery = function(preview = "preview", full = "image") {
-    images <- list.files(preview, pattern="png$")
+  make_gallery = function(preview = "preview", full = "image", preview_fmt = "jpg", full_fmt = "png") {
+    full_images <- list.files(full, pattern = paste0(full_fmt, "$"))
+    preview_images <- list.files(preview, pattern = paste0(preview_fmt, "$"))
     links <- paste0(
-      '<a href="', full, '/', images, '">',
-      '<img src="', preview, '/', images, '">',
-      '</a>'
+      '<div class="col-md-3 col-sm-6 col-xs-12">',
+      '<a href="', full, '/', full_images, '">',
+      '<img src="', preview, '/', preview_images, '">',
+      '</a>',
+      '</div>'
     )
-    cat('<div class="d-flex justify-content-between">\n')
-    cat('<div class="gal">\n')
+    cat('<div class="container-fluid">')
+    cat('  <div class="row">')
     cat(paste(links, collapse="\n"))
+    cat('  </div>\n')
     cat('</div>\n')
-    cat('</div>\n')
+  },
+
+  make_previews = function(path, fmt = "jpg") {
+
+    library(magick)
+
+    img <- dir(
+      path = file.path(path, "image"),
+      pattern = "png$",
+      recursive = TRUE
+    )
+
+    for(i in img) {
+      input <- file.path(path, "image", i)
+      output <- file.path(path, "preview", i)
+      output <- gsub("\\.png$", paste0(".", fmt), output)
+      if(!file.exists(output)) {
+        cat(input, "\n")
+        im <- magick::image_read(input)
+        im <- magick::image_resize(im, geometry = "800x")
+        magick::image_write(im, output, format = fmt)
+        Sys.sleep(1.5)
+      }
+    }
   }
 
 )
