cabe6e0a04a7711392281a6247a7ff555bd33cd0
Danielle Navarro <d.navarro@unsw.edu.au>
2020-09-20 13:58:28 +1000

    revert to plain rows

--- exampleSite/content/diziet.R
+++ exampleSite/content/diziet.R
@@ -14,11 +14,9 @@ diziet <- list(
     links <- paste0(
 
       '<div class="col-12 col-sm-12 col-md-4 col-lg-3">',
-      '<div class="card">',
       '  <a href="', full, '/', full_images, '">',
-      '    <img width="100%" class="card-img-top" src="', preview, '/', preview_images, '">',
+      '    <img width="100%" src="', preview, '/', preview_images, '">',
       '  </a>',
-      '</div>',
       '</div>'
     )
 
