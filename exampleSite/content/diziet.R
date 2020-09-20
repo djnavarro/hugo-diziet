c130124ec6f6f3c5f8141881beec6624ec54c446
Danielle Navarro <d.navarro@unsw.edu.au>
2020-09-20 14:48:44 +1000

    p tags in gal class has no padding or margin

--- exampleSite/content/diziet.R
+++ exampleSite/content/diziet.R
@@ -4,10 +4,12 @@ diziet <- list(
     full_images <- list.files(full, pattern = paste0(full_fmt, "$"))
     preview_images <- list.files(preview, pattern = paste0(preview_fmt, "$"))
     links <- paste0(
+      '<div class="gal">',
       '<div class="col-md-3 col-sm-6 col-xs-12 p-2">',
       '<a href="', full, '/', full_images, '">',
       '<img width = 100% src="', preview, '/', preview_images, '">',
       '</a>',
+      '</div>',
       '</div>'
     )
     cat('<div class="container-fluid">')
